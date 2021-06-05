840fdf84ee72cfae358b0c74a0cbf38d816f12f3
duilan <soyduilan@gmail.com>
2021-06-04 23:00:39 -0500

    agrega diseño del launchScreen

--- CoffeShop-iOS/AppDelegate.swift
+++ CoffeShop-iOS/AppDelegate.swift
@@ -12,6 +12,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        slowLaunchScreen()
         return true
+    }
+    
+    func slowLaunchScreen() {
+        sleep(1)
     }
 }
