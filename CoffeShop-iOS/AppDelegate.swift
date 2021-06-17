9dc1daca6477e7b6f18abc845e64284a98b7cd1c
duilan <soyduilan@gmail.com>
2021-06-16 19:18:23 -0500

    actualiza la apariencia el navigationBar

--- CoffeShop-iOS/AppDelegate.swift
+++ CoffeShop-iOS/AppDelegate.swift
@@ -22,12 +22,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func configureNavigationBarAppearance() {
-        // remove border
+        // Remove border bottom NavigationBar
         UINavigationBar.appearance().setBackgroundImage(UIImage(), for: .default)
         UINavigationBar.appearance().shadowImage = UIImage()
-        // change color background, items-buttons and text
-        UINavigationBar.appearance().barTintColor = CustomColors.backgroundColor
-        UINavigationBar.appearance().tintColor = CustomColors.primaryColor
-        UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor : CustomColors.primaryColor]
+        
+        // Setup BackIndicatorImage NavigationBar
+        let image = UIImage(named: AssetManager.backIndicator)
+        UINavigationBar.appearance().backIndicatorImage = image
+        UINavigationBar.appearance().backIndicatorTransitionMaskImage = image
+        
+        // Setup Text NavigationBar
+        UINavigationBar.appearance().setTitleVerticalPositionAdjustment(CGFloat(3), for: .default)
+        UINavigationBar.appearance().titleTextAttributes = [
+            .foregroundColor : CustomColors.textNavBarColor,
+            .font: UIFont.systemFont(ofSize: 22, weight: .medium)
+        ]
+        
+        // Color NavigationBar
+        UINavigationBar.appearance().barTintColor = CustomColors.textNavBarColor
+        UINavigationBar.appearance().tintColor = CustomColors.textNavBarColor
     }
 }
