b2d044c3d65f9a71ab9f37a394791de3b3204509
duilan <soyduilan@gmail.com>
2021-06-23 00:53:44 -0500

    muestra el VC adecuado respecto el estado del login en firebase

--- CoffeShop-iOS/SceneDelegate.swift
+++ CoffeShop-iOS/SceneDelegate.swift
@@ -6,22 +6,46 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     var window: UIWindow?
+    private var handleAuthStateListener: AuthStateDidChangeListenerHandle?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = createOnboardingVC()
+        window?.backgroundColor = .systemBackground
+        // observer of firebaseAuth - to show the login or home app
+        handleAuthStateListener = Auth.auth().addStateDidChangeListener { [weak self] (auth, user) in
+            guard let self = self else { return}
+            if user != nil {
+                self.window?.rootViewController = self.createTabBarController()
+            } else {
+                self.window?.rootViewController = self.createOnboardingVC()
+            }
+        }
         window?.makeKeyAndVisible()
+        
     }
     
-    func createOnboardingVC() -> UINavigationController {
+    private func createOnboardingVC() -> UINavigationController {
         let onboardingVC = OnboardingVC()
         onboardingVC.title = ""
         return UINavigationController(rootViewController: onboardingVC)
+    }
+    
+    private func createTabBarController() -> UITabBarController {
+        let tabbar = UITabBarController()
+        tabbar.viewControllers = [UIViewController()]
+        return tabbar
+    }
+        
+    func sceneDidDisconnect(_ scene: UIScene) {
+        // delete observer of firebaseAuth
+        guard let handle = handleAuthStateListener else { return  }
+        Auth.auth().removeStateDidChangeListener(handle)
     }
 }
