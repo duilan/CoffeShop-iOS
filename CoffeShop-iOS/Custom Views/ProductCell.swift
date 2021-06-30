8b50a8947d66f4f9a13dbe6ffb304485b91c82b0
duilan <soyduilan@gmail.com>
2021-06-29 20:48:40 -0500

    agrega descripcion y precio en diseño ProductCell

--- CoffeShop-iOS/Custom Views/ProductCell.swift
+++ CoffeShop-iOS/Custom Views/ProductCell.swift
@@ -12,21 +12,27 @@ class ProductCell: UITableViewCell {
     // MARK: -  Properties
     static let cellID = "ProductCell"
     private let imageProductView = UIImageView(frame: .zero)
-    private let nameProductLabel = CSTitleLabel(fontSize: 20, fontWeight: .medium, textAlignment: .left)
-        
+    private let nameProductLabel = CSTitleLabel(fontSize: 17, fontWeight: .bold, textAlignment: .left)
+    private let descProductLabel = CSTitleLabel(fontSize: 13, fontWeight: .regular, textAlignment: .left)
+    private let priceProductLabel = CSTitleLabel(fontSize: 12, fontWeight: .bold, textAlignment: .left)
+    private let infoStack = UIStackView()
+    
     // MARK: -  Init
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         setup()
         setupImageProduct()
+        setupInfoStackProduct()
         setupNameProduct()
+        setupDescProduct()
+        setupPriceProduct()
     }
     
     override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
         super.traitCollectionDidChange(previousTraitCollection)
         adjustsImageBackgroundInDarkmode()
     }
-            
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -35,34 +41,54 @@ class ProductCell: UITableViewCell {
     public func configure(with product: Product) {
         nameProductLabel.text = product.name
         imageProductView.image = UIImage(named: product.image_url)
+        descProductLabel.text = product.description
+        priceProductLabel.text = String(format: "$%.02f", product.price)
     }
     
     // MARK: -  Private Methods
     private func setup() {
         self.backgroundColor = CustomColors.backgroundColor
+        self.selectionStyle = .none
+        self.accessoryType = .disclosureIndicator
     }
     
     private func setupImageProduct() {
-        #warning("CREATE CUSTOM VIEW FOR IMG WITH URL")
+        #warning("CREATE CUSTOM VIEW FOR IMG WITH URL")            
         contentView.addSubview(imageProductView)
         imageProductView.translatesAutoresizingMaskIntoConstraints = false
         imageProductView.layer.cornerCurve = .continuous
         imageProductView.layer.cornerRadius = 35
         adjustsImageBackgroundInDarkmode()
         //Constrains
-        imageProductView.widthAnchor.constraint(equalToConstant: 90).isActive = true
-        imageProductView.heightAnchor.constraint(equalToConstant: 90).isActive = true
+        imageProductView.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        imageProductView.heightAnchor.constraint(equalToConstant: 100).isActive = true
         imageProductView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16).isActive = true
         imageProductView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
     }
     
+    private func setupInfoStackProduct() {
+        contentView.addSubview(infoStack)
+        infoStack.axis = .vertical
+        infoStack.distribution = .fillEqually
+        infoStack.alignment = .fill
+        infoStack.spacing = 0
+        infoStack.anchor(top: contentView.topAnchor, left: imageProductView.trailingAnchor, right: contentView.trailingAnchor, bottom: contentView.bottomAnchor, paddingTop: 16, paddingLeft: 16, paddingRight: 16, paddingBottom: 16, width: 0, height: 0)
+    }
+    
     private func setupNameProduct() {
-        contentView.addSubview(nameProductLabel)
-        //Constrains
+        infoStack.addArrangedSubview(nameProductLabel)
         nameProductLabel.textColor = CustomColors.textPrimaryColor
-        nameProductLabel.heightAnchor.constraint(equalToConstant: 40 ).isActive = true
-        nameProductLabel.leadingAnchor.constraint(equalTo: imageProductView.trailingAnchor,constant: 16).isActive = true
-        nameProductLabel.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true        
+        nameProductLabel.numberOfLines = 2
+    }
+    
+    private func setupDescProduct() {
+        infoStack.addArrangedSubview(descProductLabel)
+        descProductLabel.textColor = .secondaryLabel
+        descProductLabel.minimumScaleFactor = 1
+        descProductLabel.numberOfLines = 0
+    }
+    private func setupPriceProduct() {
+        infoStack.addArrangedSubview(priceProductLabel)
     }
     
     private func adjustsImageBackgroundInDarkmode() {
--- CoffeShop-iOS/Screens/ProductListVC.swift
+++ CoffeShop-iOS/Screens/ProductListVC.swift
@@ -12,11 +12,11 @@ class ProductListVC: UIViewController {
     // MARK: -  Properties
     private let tableView = UITableView()
     private var products: [Product] = [
-        Product(name: "Americano", price: 10.0, image_url: "Americano", description: "desc", isAvailable: true),
-        Product(name: "Cappuccino", price: 10.0, image_url: "Cappuccino", description: "desc", isAvailable: true),
-        Product(name: "Iced Latte", price: 10.0, image_url: "IcedLatte", description: "desc", isAvailable: true),
-        Product(name: "Macchiato", price: 10.0, image_url: "Macchiato", description: "desc", isAvailable: true),
-        Product(name: "Mocha", price: 10.0, image_url: "Mocha", description: "desc", isAvailable: true)
+        Product(name: "Expresso Americano", price: 12.50, image_url: "Americano", description: "El café americano se prepara diluyendo un espresso con agua caliente", isAvailable: true),
+        Product(name: "Cappuccino", price: 17.00, image_url: "Cappuccino", description: "El café espresso y la espuma de leche se combinan para crear el clásico cappuccino", isAvailable: true),
+        Product(name: "Iced Latte", price: 12.50, image_url: "IcedLatte", description: "Despertar con un café frío nunca fue tan delicioso. Espuma de leche, espresso, y estás listo para disfrutar de una refrescante café frío para cualquier momento del día.", isAvailable: true),
+        Product(name: "Macchiato", price: 17.00, image_url: "Macchiato", description: "Un café de tueste suave pero lleno de sabor. Añade un poco de espuma de leche y obtendrás un delicioso y cremoso macchiato.", isAvailable: true),
+        Product(name: "Mocha", price: 22.00, image_url: "Mocha", description: "Un poco de crema batida y una pizca de virutas de chocolate lo son todo para este rico clásico achocolatado", isAvailable: true)
     ]
     
     // MARK: -  Lifecycle
@@ -39,7 +39,7 @@ class ProductListVC: UIViewController {
         tableView.frame = view.bounds
         tableView.delegate = self
         tableView.dataSource = self
-        tableView.rowHeight = 120        
+        tableView.rowHeight = 140
         tableView.backgroundColor = CustomColors.backgroundColor
         tableView.register(ProductCell.self, forCellReuseIdentifier: ProductCell.cellID)
     }
