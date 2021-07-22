48b3407ddb99ebec8aef368d41e2664541495a81
duilan <soyduilan@gmail.com>
2021-07-22 16:45:04 -0500

    actualiza diseño del headersection

--- CoffeShop-iOS/Custom Views/Views/SectionTitleTableView.swift
+++ CoffeShop-iOS/Custom Views/Views/SectionTitleTableView.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class SectionTitleTableView: UIView {
     private let titleBadge = CSBadgeView()
+    private let separatorView = UIView(frame: .zero)
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -27,11 +28,27 @@ class SectionTitleTableView: UIView {
     
     private func setupUI() {
         backgroundColor = CustomColors.backgroundColorSecondary
+        setupSeparator()
+        setupTitleBadge()
+    }
+    
+    private func setupTitleBadge() {
         addSubview(titleBadge)
         titleBadge.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
         titleBadge.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
         titleBadge.widthAnchor.constraint(greaterThanOrEqualToConstant: 100).isActive = true
         titleBadge.heightAnchor.constraint(equalToConstant: 30).isActive = true
+    }
+    
+    private func setupSeparator(){
+        addSubview(separatorView)
+        separatorView.layer.cornerRadius = 6
+        separatorView.backgroundColor = CustomColors.backgroundColorTertiary
+        separatorView.translatesAutoresizingMaskIntoConstraints = false
+        separatorView.heightAnchor.constraint(equalToConstant: 12).isActive = true
+        separatorView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20).isActive = true
+        separatorView.trailingAnchor.constraint(equalTo: trailingAnchor, constant:-20).isActive = true
+        separatorView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
     }
     
 }
