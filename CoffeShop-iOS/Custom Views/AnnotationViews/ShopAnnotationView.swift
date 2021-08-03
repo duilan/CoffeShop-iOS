dde20b3fd1284baaf6c68feabf8f13fb98c7b423
duilan <soyduilan@gmail.com>
2021-08-02 23:47:58 -0500

    agrega custom pin para mostrar shops en mapa

---
+++ CoffeShop-iOS/Custom Views/AnnotationViews/ShopAnnotationView.swift
@@ -0,0 +1,41 @@
+//
+//  ShopAnnotationView.swift
+//  CoffeShop-iOS
+//
+//  Created by Duilan on 02/08/21.
+//
+
+import UIKit
+import MapKit
+
+class ShopAnnotationView: MKAnnotationView {
+    
+    static let annotationID = "ShopAnnotationView"
+    
+    override var isSelected: Bool {
+        didSet {
+            if isSelected {
+                image = UIImage(named: "shopPinSelected")
+            } else {
+                image = UIImage(named: "shopPinUnselected")
+            }
+        }
+    }
+    
+    override init(annotation: MKAnnotation?, reuseIdentifier: String?) {
+        super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
+        setupImage()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupImage() {
+        image = UIImage(named: "shopPinUnselected")
+        if let imageHeight = image?.size.height {
+            centerOffset = CGPoint(x: 0, y: -imageHeight / 2)
+        }
+    }
+    
+}
