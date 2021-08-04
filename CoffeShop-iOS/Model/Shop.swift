d48f897c82f259471b1a55c77f69bccb68d21ae0
duilan <soyduilan@gmail.com>
2021-08-03 21:50:58 -0500

    agrega modelo de tiendas

---
+++ CoffeShop-iOS/Model/Shop.swift
@@ -0,0 +1,16 @@
+//
+//  Shop.swift
+//  CoffeShop-iOS
+//
+//  Created by Duilan on 03/08/21.
+//
+
+import Foundation
+
+struct Shop {
+    let id: String
+    let name: String
+    let address: String
+    let latitude: Double
+    let longitude: Double
+}
---
+++ CoffeShop-iOS/Model/ShopModel.swift
@@ -0,0 +1,17 @@
+//
+//  ShopModel.swift
+//  CoffeShop-iOS
+//
+//  Created by Duilan on 03/08/21.
+//
+
+import Foundation
+
+struct ShopModel {
+    let shops: [Shop] = [
+        Shop(id: "1", name: "Santo Tomás", address: "Calle Santo Tomás, Cuauhtémoc, 06060 Ciudad de México, México", latitude: 19.427271821196612, longitude: -99.12627266528202),
+        Shop(id: "2", name: "Viaducto 1", address: "Viaducto Presidente Miguel Alemán, Piedad Narvarte, 03000 Ciudad de México, México", latitude: 19.4038828686858, longitude: -99.15681399541693),
+        Shop(id: "3", name: "Areonáutica", address: "Calle Sur 109, Aeronáutica militar, 15970 Ciudad de México, México", latitude: 19.42111096383475, longitude: -99.11528777258876)
+    ]
+    
+}
