f1fb7c124be61d07e9a4c3a5bfb738f58a838a2c
duilan <soyduilan@gmail.com>
2021-08-21 23:59:48 -0500

    agrega celda para noticias

---
+++ CoffeShop-iOS/Custom Views/Cells/NewsletterCell.swift
@@ -0,0 +1,42 @@
+//
+//  NewsletterCell.swift
+//  CoffeShop-iOS
+//
+//  Created by Duilan on 21/08/21.
+//
+
+import UIKit
+
+class NewsletterCell: UICollectionViewCell {
+    
+    static var cellID = "NewsletterCell"
+    private let imageView = UIImageView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupImageView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    func set(image: String?) {
+        imageView.image = UIImage(named: image ?? AssetManager.newsletter)
+    }
+    
+    private func setupImageView() {
+        contentView.addSubview(imageView)
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 20
+        imageView.layer.cornerCurve = .continuous
+        imageView.layer.borderWidth = 2
+        imageView.layer.borderColor = UIColor.white.cgColor
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.anchor(top: contentView.topAnchor, left: contentView.leadingAnchor, right: contentView.trailingAnchor, bottom: contentView.bottomAnchor, paddingTop: 0, paddingLeft: 0, paddingRight: 0, paddingBottom: 0, width: 0, height: 0)
+        
+    }
+}
--- CoffeShop-iOS/Screens/Home/HomeVC.swift
+++ CoffeShop-iOS/Screens/Home/HomeVC.swift
@@ -9,14 +9,114 @@ import UIKit
 
 class HomeVC: UIViewController {
     
+    private var collectionView: UICollectionView!
+    private var dataSource: UICollectionViewDiffableDataSource<SectionLayotKind, Int>!
+    
+    private enum SectionLayotKind {
+        case main, news
+    }
+    
     // MARK: -  Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
+        setupCollectionView()
     }
     
     private func setup() {
         view.backgroundColor = CustomColors.backgroundColor
+        title = "Welcome Adrian"
     }
+    
+    private func setupCollectionView(){
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createSmallLayout())
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.backgroundColor = .none
+        collectionView.register(AnnouncementCell.self, forCellWithReuseIdentifier: AnnouncementCell.cellID)
+        collectionView.register(NewsletterCell.self, forCellWithReuseIdentifier: NewsletterCell.cellID)
+        collectionView.dataSource = self
+        view.addSubview(collectionView)
+    }
+    
+}
+
+extension HomeVC {
+    private func createLayout() -> UICollectionViewLayout {
+        let itemLargeSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+        let itemLarge = NSCollectionLayoutItem(layoutSize: itemLargeSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.9), heightDimension: .fractionalWidth(0.9))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [itemLarge])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = NSDirectionalEdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 20)
+        section.orthogonalScrollingBehavior = .groupPagingCentered
+        section.interGroupSpacing = 10
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        
+        
+        return layout
+    }
+    
+    private func configureDataSource() {
+        
+    }
+    
+    private func createSmallLayout() -> UICollectionViewLayout {
+        
+        let itemSmallSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+        let itemSmall = NSCollectionLayoutItem(layoutSize: itemSmallSize)
+        
+        let groupSmallSize = NSCollectionLayoutSize(widthDimension: .absolute(260), heightDimension: .absolute(120))
+        let groupSmall = NSCollectionLayoutGroup.horizontal(layoutSize: groupSmallSize, subitems: [itemSmall])
+        
+        let section = NSCollectionLayoutSection(group: groupSmall)
+        section.contentInsets = NSDirectionalEdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 20)
+        section.orthogonalScrollingBehavior = .continuousGroupLeadingBoundary
+        section.interGroupSpacing = 20
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+            
+        
+        return layout
+    }
+
+}
+
+extension HomeVC: UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 6
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: AnnouncementCell.cellID, for: indexPath) as? AnnouncementCell else {
+//            return AnnouncementCell()
+//        }
+        
+//        if indexPath.section == 0 {
+//            cell.set(image: AssetManager.gooddaycoffe)
+//        } else {
+//            cell.set(image: AssetManager.marketingNews)
+//        }
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: NewsletterCell.cellID, for: indexPath) as? NewsletterCell else {
+            return NewsletterCell()
+        }
+        
+        if indexPath.section == 0 {
+            cell.set(image: AssetManager.newsletter)
+        } else {
+            cell.set(image: AssetManager.marketingNews)
+        }
+        
+        return cell
+    }
+    
     
 }
