2432fdeb418ad6079d480e8f283afde233c53b79
duilan <soyduilan@gmail.com>
2021-08-25 23:33:00 -0500

    crea vc para el detalle de anuncios

---
+++ CoffeShop-iOS/Screens/Home/AnnouncementDetailVC.swift
@@ -0,0 +1,21 @@
+//
+//  AnnouncementDetailVC.swift
+//  CoffeShop-iOS
+//
+//  Created by Duilan on 25/08/21.
+//
+
+import UIKit
+
+class AnnouncementDetailVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+    
+    private func setup(){
+        title = "Announcement Title"
+    }
+    
+}
