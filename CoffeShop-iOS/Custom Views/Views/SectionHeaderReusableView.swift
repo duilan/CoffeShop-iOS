65122e35d222877fd6d802366ae2fa30d8b2b933
duilan <soyduilan@gmail.com>
2021-08-28 23:22:31 -0500

    agrega boton show all en el header de seccion  para anuncios

--- CoffeShop-iOS/Custom Views/Views/SectionHeaderReusableView.swift
+++ CoffeShop-iOS/Custom Views/Views/SectionHeaderReusableView.swift
@@ -11,9 +11,12 @@ class SectionHeaderReusableView: UICollectionReusableView {
     static let reusableID = "SectionHeaderReusableView"
     private let titleLabel = CSTitleLabel(fontSize: 16, fontWeight: .black, textAlignment: .left)
     
+    public let showAllButton = CSButtonPlain("Show All →")
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setup()
+        setupShowAllButton()
     }
     
     required init?(coder: NSCoder) {
@@ -30,6 +33,14 @@ class SectionHeaderReusableView: UICollectionReusableView {
         titleLabel.text = "No section title"
         titleLabel.textColor = CustomColors.textPrimaryColor
         titleLabel.anchor(top: topAnchor, left: leadingAnchor, right: trailingAnchor, bottom: bottomAnchor, paddingTop: 0, paddingLeft: 0, paddingRight: 0, paddingBottom: 0, width: 0, height: 0)
+    }
+    
+    private func setupShowAllButton() {
+        addSubview(showAllButton)
+        showAllButton.backgroundColor = .clear
+        showAllButton.titleLabel?.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        showAllButton.setTitleColor(CustomColors.primaryColor, for: .normal)
+        showAllButton.anchor(top: topAnchor, left: nil, right: trailingAnchor, bottom: bottomAnchor, paddingTop: 0, paddingLeft: 0, paddingRight: 0, paddingBottom: 0, width: 85, height: 0)
     }
     
 }
