d2f9c64df48c6f63a5630b06c90e47099b433021
duilan <soyduilan@gmail.com>
2021-09-01 21:12:58 -0500

    arregla la desaparicion del LargeTitle cuando se llega desde la lista de todos los anuncios

--- CoffeShop-iOS/Screens/Home/AnnouncementDetailVC.swift
+++ CoffeShop-iOS/Screens/Home/AnnouncementDetailVC.swift
@@ -40,7 +40,7 @@ class AnnouncementDetailVC: UIViewController {
     private func setup(){
         title = announcement.title
         view.backgroundColor = CustomColors.backgroundColor
-        navigationController?.navigationBar.prefersLargeTitles = false
+        navigationItem.largeTitleDisplayMode = .never
     }
     
     private func setupScrollView() {
