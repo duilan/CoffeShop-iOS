d235181ea7c145cb0de4fdbec36bba99a29a5945
duilan <soyduilan@gmail.com>
2021-09-03 00:16:32 -0500

    extiende metodo para mostrar/ocultar activityIndicator sobre un VC

--- CoffeShop-iOS/Extensions/UIViewController+Ext.swift
+++ CoffeShop-iOS/Extensions/UIViewController+Ext.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+fileprivate var containerLoadingView: UIView!
+
 extension UIViewController {
     func presetCSAlertVC(title: String, message: String, buttonTitle: String) {
         DispatchQueue.main.async {
@@ -14,6 +16,34 @@ extension UIViewController {
             alert.modalPresentationStyle = .overFullScreen
             alert.modalTransitionStyle = .crossDissolve
             self.present(alert, animated: true)
+        }
+    }
+    
+    func showLoadingView() {
+        containerLoadingView = UIView(frame: self.view.bounds)
+        view.addSubview(containerLoadingView)
+        
+        containerLoadingView.backgroundColor = CustomColors.backgroundColor
+        containerLoadingView.alpha = 0
+        
+        UIView.animate(withDuration: 0.25) {
+            containerLoadingView.alpha = 0.8
+        }
+        
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+        containerLoadingView.addSubview(activityIndicator)
+        
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        activityIndicator.startAnimating()
+    }
+    
+    func dismissLoadingView() {
+        DispatchQueue.main.async {
+            containerLoadingView.removeFromSuperview()
+            containerLoadingView = nil
         }
     }
 }
